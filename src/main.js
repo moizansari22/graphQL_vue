2a09913110697dc1a983fdfda226773b2a4a3a14
moizansari22 <113978056+moizansari22@users.noreply.github.com>
2022-11-21 12:56:11 +0500

    graphQL in vueJS

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import App from './App.vue'
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'https://rickandmortyapi.com/graphql',
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.mount('#app');

466958e429e11ac637479ddf62bfb57df63ee3cf
moizansari22 <113978056+moizansari22@users.noreply.github.com>
2022-11-21 18:27:06 +0500

    GraphQL in vueJS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,36 @@
 <template>
-  <p v-if="error">Something went wrong...</p>
-  <p v-if="loading">Loading...</p>
-  <p v-else v-for="character in result.characters.results" :key="character.id">
-    id: {{ character.id}} ---------- Name:{{ character.name }} ------------ images:{{ character.image }}
-  </p>
-  <div></div>
+  <div v-if="loading">Loading...</div>
+
+  <div v-else-if="error">Error: {{ error.message }}</div>
+  <ul v-else-if="result.characters.results">
+    <li v-for="user of result.characters.results" :key="user.id">
+      {{ user.id}} ------------------------ {{ user.name }} --------------------- {{ user.image }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
 
-const CHARACTERS_QUERY = gql`
-  query Characters {
-    characters {
-      results{
-        id
-        name
-        image
-      }
-    }
-  }
-`;
 export default {
-  name: 'App',
   setup () {
-    const { result, loading, error } = useQuery(CHARACTERS_QUERY);
+    const { result, loading, error } = useQuery(gql`
+    query Characters {
+      characters {
+        results{
+          id
+          name
+          image
+        }
+      }
+    }  
+  `)
     return {
       result,
-      loading, 
-      error
+      loading,
+      error,
     }
-  }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
